42d07fc591dbc783488d8360fb4e0e49305bd9b5
0x3EFFFC8 <X3R0_SEXY@outlook.com>
2025-02-13 20:30:28 -0500

    Create Express server

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+//Middleware
+app.use(express.json());
+
+//Start the server
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
+
